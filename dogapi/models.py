5a8fdd4e3d6c9ec1550dc8f4c691fc71bffdc167
santhoshi <saisanthoshiarcot@gmail.com>
2024-12-18 10:48:48 +0530

    Save local changes before pull

---
+++ dogapi/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Breed(models.Model):
+    SIZE_CHOICES = [
+        ('Tiny', 'Tiny'),
+        ('Small', 'Small'),
+        ('Medium', 'Medium'),
+        ('Large', 'Large'),
+    ]
+
+    name = models.CharField(max_length=100)
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES)
+    friendliness = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    trainability = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    sheddingamount = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    exerciseneeds = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=10)
+    color = models.CharField(max_length=30)
+    favoritefood = models.CharField(max_length=50)
+    favoritetoy = models.CharField(max_length=50)
+
+
